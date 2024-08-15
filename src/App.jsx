edb057ca81fed0bea77d941fcaba5cc580b18271
Yiim <ilyimyan@gmail.com>
2024-08-15 22:43:54 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import React from "react"
+import {buddha, diogenes, kurt, karl, marcus, nietzsche, camus, confucius, plato, aristotle } from './assets/author/pic.js'
 
 function App() {
+
 
   return (
     <>
       <div className="bg-gradient-to-r from-blue-300 to-violet-400 w-full flex justify-center items-center h-[100vh] text-center">
 
         {/* quotes box */}
-        <div className="w-full rounded-xl h-[350px] bg-white">
-          <div className="text-center mx-auto w-[300px] mt-20">
+        <div className="w-full rounded-xl h-[400px] bg-white">
+          <div className="mx-auto overflow-hidden w-[150px] h-[150px] mt-2">
+          <img src={buddha} alt="" />
+          </div>
+          <div className="text-center mx-auto w-[300px] mt-2">
           <p className="font-medium">However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?</p>
           </div>
-          <div className="mt-16 text-right mr-5">
+          <div className="mt-5 text-right mr-5">
             <div>- Siddharta Gautama</div>
           </div>
-            <button className="bg-gradient-to-r w-full p-3 mt-3 rounded-full text-2xl font-medium text-white ">next quote</button>
+            <button className="bg-gradient-to-r w-full p-3 mt-2 rounded-full text-2xl font-medium text-white ">next quote</button>
         </div>
 
       </div>
---
+++ src/assets/author/Pic.js
@@ -0,0 +1,12 @@
+import buddha from '../author/buddha.jpg';
+import diogenes from '../author/Diogenes.png';
+import kurt from '../author/kurt.jpeg';
+import karl from '../author/karl.jpeg';
+import marcus from '../author/marcus aurelius.jpeg';
+import nietzsche from '../author/Nietzsche.jpg';
+import camus from '../author/camus.jpeg';
+import confucius from '../author/Confucius.jpg';
+import plato from '../author/plato.jpeg';
+import aristotle from '../author/aristotle.jpeg';
+
+export {buddha, diogenes, kurt, karl, marcus, nietzsche, camus, confucius, plato, aristotle };
