1f77704cbef5d8940064944fce4656856e1f3776
Yiim <ilyimyan@gmail.com>
2024-08-15 21:15:30 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ function App() {
 
   return (
     <>
-      <div className="bg-gradient-to-r from-blue-300 border-t-violet-300 w-full flex justify-center items-center h-[100vh]">
+      <div className="bg-gradient-to-r from-blue-300 border-t-violet-400 w-full flex justify-center items-center h-[100vh]">
 
         {/* quotes box */}
-        <div className="w-[500px] h-[300px] bg-white">
-          <div className="text-center mx-auto w-[300px]">
-          <p className="text-2xl">"However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?</p>
+        <div className="w-full rounded-xl h-[300px] bg-white">
+          <div className="text-center mx-auto w-[300px] mt-20">
+          <p className="">"However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?</p>
           </div>
+
         </div>
 
       </div>
