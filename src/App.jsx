e749f57517ecdb4a4cbbfd597770898e2f0524db
Yiim <ilyimyan@gmail.com>
2024-08-15 20:58:37 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-gradient-to-r from-blue-300 border-t-violet-300 w-full flex justify-center items-center h-[100vh]">
+
+        {/* quotes box */}
+        <div className="w-[500px] h-[300px] bg-white">
+          <div className="text-center mx-auto w-[300px]">
+          <p className="text-2xl">"However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?</p>
+          </div>
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
