e75e9130a12902608b95198588d44cf232c46f2f
Yiim <ilyimyan@gmail.com>
2024-08-15 21:29:38 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react"
+import quote from './assets/quote.png'
 
 function App() {
 
   return (
     <>
-      <div className="bg-gradient-to-r from-blue-300 border-t-violet-400 w-full flex justify-center items-center h-[100vh]">
+      <div className="bg-gradient-to-r from-blue-300 border-t-violet-400 w-full flex justify-center items-center h-[100vh] text-center">
 
         {/* quotes box */}
         <div className="w-full rounded-xl h-[300px] bg-white">
           <div className="text-center mx-auto w-[300px] mt-20">
-          <p className="">"However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?</p>
+          <p className="font-medium">However many holy words you read, however many you speak, what good will they do you if you do not act on upon them?</p>
           </div>
-
+          <div className="mt-16 text-right mr-5">
+            <div>- Siddharta Gautama</div>
+          </div>
         </div>
 
       </div>
